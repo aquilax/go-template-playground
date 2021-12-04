b044116753c0568431dcc8ba43623de76aff63cb
aquilax <aquilax@gmail.com>
2021-12-04 10:05:34 +0100

    Updated sw cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   globDirectory: "public/",
-  globPatterns: ["**/*.{ico,html,js,svg,png}"],
+  globPatterns: ["**/*.{ico,html,js,svg,png,json}"],
   swDest: "public/sw.js",
   ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
 };
